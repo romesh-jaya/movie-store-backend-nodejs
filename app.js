ae76746c646be2c2415e8242f1abcc24e337ef2a
fm_romesh <romesh@favoritemedium.com>
2020-10-17 09:20:58 +0530

    feat: auth0

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 //Routes
-const moviesRoutes = require("./routes/movies");
-const settingsRoutes = require("./routes/settings");
+const moviesRoutes = require('./routes/movies');
+const settingsRoutes = require('./routes/settings');
 
 const app = express();
 
@@ -13,45 +15,65 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 // Following prints queries
-mongoose.set("debug", (collectionName, method, query, doc) => {
-    const currentTime = new Date();
-    console.log(`${currentTime.toDateString()} ${currentTime.toLocaleTimeString()} ${collectionName}.${method}`, JSON.stringify(query), doc);
+mongoose.set('debug', (collectionName, method, query, doc) => {
+  const currentTime = new Date();
+  console.log(
+    `${currentTime.toDateString()} ${currentTime.toLocaleTimeString()} ${collectionName}.${method}`,
+    JSON.stringify(query),
+    doc
+  );
 });
 
 mongoose
-    .connect(
-        process.env.MONGOENDPOINT
-    )
-    .then(() => {
-        console.log("Connected to database!");
-    })
-    .catch(() => {
-        console.log("Connection failed, exiting!");
-        process.exit(-1);
-    });
+  .connect(process.env.MONGOENDPOINT)
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed, exiting!');
+    process.exit(-1);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
 });
 
 //Introduction message
 app.get('/', function (req, res) {
-    res.send("Node server is up.");
+  res.send('Node server is up.');
 });
 
-app.use("/movies", moviesRoutes);
-app.use("/settings", settingsRoutes);
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://movie-shop.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'https://movie-shop-backend',
+  issuer: 'https://movie-shop.us.auth0.com/',
+  algorithms: ['RS256'],
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
+});
+
+app.use('/movies', moviesRoutes);
+app.use('/settings', settingsRoutes);
 
 module.exports = app;
