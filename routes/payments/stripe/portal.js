8b9b2d3b0653d48c2a7c337ac446a937799724be
romesh-jaya <monty.rom.jay@gmail.com>
2022-05-25 12:29:29 +0530

    feat: manage subscription

---
+++ routes/payments/stripe/portal.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY);
+const stripeCommon = require('./common');
+
+router.post('/create-customer-portal-session', async (req, res) => {
+  const { redirectFromCheckoutURLCancelled } = req.body;
+  const { userEmail } = req;
+  let savedPaymentCustomer = '';
+
+  try {
+    savedPaymentCustomer = await stripeCommon.getOrCreatePaymentCustomer(
+      userEmail
+    );
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Create Payment Customer failed : ' + error.message,
+    });
+  }
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: savedPaymentCustomer.paymentCustomerIdStripe,
+    return_url: redirectFromCheckoutURLCancelled,
+  });
+
+  res.json({ url: session.url });
+});
+
+module.exports = router;
